0afca22e09afbf079e86c7f94d7ef68790b10e65
17302010035 <37619259+17302010035@users.noreply.github.com>
2018-05-28 01:46:49 -0500

    Add files via upload

--- functions.inc.php
+++ functions.inc.php
@@ -10,40 +10,6 @@ function generateLink($url, $label, $class) {
 
 function outputPostRow($number)  {
     include("travel-data.inc.php");
-    $postId = "postId" . $number;
-    $userId = "userId" . $number;
-    $userName = "userName" . $number;
-    $date = "date" . $number;
-    $thumb = "thumb" . $number;
-    $title = "title" . $number;
-    $excerpt = "excerpt" . $number;
-    $reviewsNum = "reviewsNum" . $number;
-    $reviewsRating = "reviewsRating" . $number;
-    $rateStarsNumber = '';
-    $rateStarsNumberAnother = '';
-    for ($x = 0; $x < $$reviewsRating; $x++){
-        $rateStarsNumber .='<img src="images/star-gold.svg" width="16" />';
-        $rateStarsNumber .=' ';
-    }
-    for ($x = 0; $x < 5 - $$reviewsRating; $x++){
-        $rateStarsNumberAnother .= '<img src="images/star-white.svg" width="16" />';
-        $rateStarsNumberAnother .=' ';
-    }
-    echo '<div class="row">
-                <div class="col-md-4">
-                    <a href="images/" '. $$thumb . '><img src="images/'. $$thumb . '" alt="'.$$title .'"/></a>
-                </div>
-                <div class="col-md-8"> 
-                     <h2>'. $$title .'</h2>
-                     <div class="details">Posted by <a href="" class="">'. $$userName .'</a>
-                         <span class="pull-right">'. $$date .'</span>
-                         <p class="ratings">
-                         ' .$rateStarsNumber.$rateStarsNumberAnother .$$reviewsNum.' Reviews</p>
-                     </div>
-                     <p class="excerpt">'. $$excerpt .'</p>
-                     <p><a href="" class="btn btn-primary btn-sm">Read more</a></p>
-                </div>
-           </div><hr/> ';
 }
 
 /*
